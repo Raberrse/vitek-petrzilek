2dcbf1135e79ffa54278744f8766e27148ff60b1
Raberrse <ogelf1994@gmail.com>
2024-01-11 18:57:38 +0100

    Test video fix

--- components/main/HeroVideo.tsx
+++ components/main/HeroVideo.tsx
@@ -1,34 +1,29 @@
 import React from 'react'
+
 export default function HeroVideo() {
   return (
-    <>
-
-    {/* Desktop  */}
-      <video
+    <video
         playsInline
         controls={false}
         width={'100%'}
         id="myVideo" 
+        autoPlay
+        muted
+        loop
+        className='min-h-[450px] h-screen object-cover relative'
+    >
+        <source
+        className=""
         src="/hero_video_lg.mp4"
-        autoPlay
-        muted
-        loop
-        className='min-h-[450px] h-screen object-cover relative max-md:hidden'
-      />
-
-    {/* Mobile */}
-      <video
-        playsInline
-        controls={false}
-        width={'100%'}
-        id="myVideo" 
+        type="video/mp4"
+        media='(min-width: 764px)'
+        />
+        <source
+        className=""
         src="/hero_video_md.mp4"
-        autoPlay
-        muted
-        loop
-        className='min-h-[450px] h-screen object-cover relative md:hidden'
-      />
-    </>
-        
+        type="video/mp4"
+        media='(max-width: 764px)'
+        />
+    </video>
   )
 }

71d19357e2c94fcb69ed0bd2c53411d12dd57ea3
Raberrse <ogelf1994@gmail.com>
2024-01-11 19:02:47 +0100

    test 2

--- components/main/HeroVideo.tsx
+++ components/main/HeroVideo.tsx
@@ -12,17 +12,19 @@ export default function HeroVideo() {
         loop
         className='min-h-[450px] h-screen object-cover relative'
     >
-        <source
-        className=""
-        src="/hero_video_lg.mp4"
-        type="video/mp4"
-        media='(min-width: 764px)'
-        />
+        
         <source
         className=""
         src="/hero_video_md.mp4"
         type="video/mp4"
         media='(max-width: 764px)'
+        />
+
+<source
+        className=""
+        src="/hero_video_lg.mp4"
+        type="video/mp4"
+        media=''
         />
     </video>
   )
